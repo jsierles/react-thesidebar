671541f747ebf64dd01ab4be9ab032ced218cab9
Daniele Zannotti <d.zannotti@me.com>
2015-03-30 00:05:55 +0100

    matched properties in react component

--- sidemenu.js
+++ sidemenu.js
@@ -1,48 +1,39 @@
 var React = require('react-native');
+var StyleSheet = React.StyleSheet;
 var merge = require('merge');
 var ReactIOSViewAttributes = require('ReactIOSViewAttributes');
 var createReactIOSNativeComponentClass = require('createReactIOSNativeComponentClass');
 
+var styles = StyleSheet.create({
+    container: {
+        flex: 1
+  }
+});
+
 var SidebarLeftView = createReactIOSNativeComponentClass({
-    validAttributes: merge(
-        ReactIOSViewAttributes.UIView, {
-        menu: true,
-        content: true
-    }),
+    validAttributes: ReactIOSViewAttributes.UIView,
     uiViewClassName: 'SidebarLeftView',
 });
 
 var SidebarContentView = createReactIOSNativeComponentClass({
-    validAttributes: merge(
-        ReactIOSViewAttributes.UIView, {
-        menu: true,
-        content: true
-    }),
+    validAttributes: ReactIOSViewAttributes.UIView,
     uiViewClassName: 'SidebarContentView',
 });
 
 var SidebarMainView = createReactIOSNativeComponentClass({
-    validAttributes: merge(
-        ReactIOSViewAttributes.UIView, {
-        menu: true,
-        content: true
-    }),
+    validAttributes: ReactIOSViewAttributes.UIView,
     uiViewClassName: 'SidebarMainView',
 });
 
 var SideMenu = React.createClass({
-    propTypes: {
-        menuView: React.PropTypes.node,
-        mainView: React.PropTypes.node
-    },
     render: function() {
         return (
-            <SidebarMainView>
-                <SidebarLeftView>
-                    {this.props.menuView}
+            <SidebarMainView style={styles.container}>
+                <SidebarLeftView style={styles.container}>
+                    {this.props.menu}
                 </SidebarLeftView>
-                <SidebarContentView>
-                    {this.props.mainView}
+                <SidebarContentView style={styles.container}>
+                    {this.props.content}
                 </SidebarContentView>
             </SidebarMainView>
         );
