6d3eb0b162b19162bb4beaddb76561776f1e1c0e
Daniele Zannotti <d.zannotti@me.com>
2015-03-30 00:13:24 +0100

    fixed layouting

--- sidemenu.js
+++ sidemenu.js
@@ -6,8 +6,12 @@ var createReactIOSNativeComponentClass = require('createReactIOSNativeComponentC
 
 var styles = StyleSheet.create({
     container: {
-        flex: 1
-  }
+        position: "absolute", 
+        top: 0, 
+        bottom: 0, 
+        left: 0, 
+        right: 0
+    }
 });
 
 var SidebarLeftView = createReactIOSNativeComponentClass({
